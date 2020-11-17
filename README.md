8692ef8ee272fdf9832de42d5c82fc09158139aa
Sebastian Pająk <spconv@gmail.com>
2020-11-17 16:27:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Countries & Flags
+
+ - List of countries (countries.csv) including basic data (area, population..);
+ - SVG national flags (from Wikipedia);
+ - SVG flags icons (rectangle 3:2, round, square).
+
+All countries and territories from [ISO_3166](https://en.wikipedia.org/wiki/ISO_3166) standard.
+
+# Demo
+
+[See & download all flags](https://www.spajak.blue/world/flags)
+
+# Icons colors
+
+```
+#ffda44 - Yellow
+#ff9811 - Orange
+#d80027 - Red
+#a2001d - Dark Red
+#d57800 - Brown
+#6da544 - Green
+#496e2d - Dark Green
+#0052b4 - Blue
+#338af3 - Dark Blue
+#f0f0f0 - White
+#acabb1 - Gray (will change to #acabaf)
+#000000 - Black
+#751a46 - Quatar flag (QA)
+```
+
+# License
+
+MIT
---
+++ build.py
@@ -0,0 +1,48 @@
+import os
+from pathlib import Path
+from itertools import chain
+import cairosvg
+
+SRC = Path('src')
+SRC_ICONS = SRC / 'icons'
+DIST = Path('dist')
+SHAPES = ['rect', 'round', 'square']
+SIZE = 200 # Set to None to disable PNG creation
+COLORS = {
+    'Yeallow': '#ffda44',
+    'Orange': '#ff9811',
+    'Red': '#d80027',
+    'Dark Red': '#a2001d',
+    'Brown': '#d57800',
+    'Green': '#6da544',
+    'Dark Green': '#496e2d',
+    'Blue': '#0052b4',
+    'Dark Blue': '#338af3',
+    'White': '#f0f0f0',
+    'Gray': '#acabb1',
+    'Black': '#000000',
+    'Quatar (QA) flag': '#751a46'
+}
+# TODO:
+# change Gray to #acabaf
+# generate color map
+
+if not DIST.is_dir():
+    DIST.mkdir()
+
+def svgo(input_, output):
+    os.system(f'svgo -p 3 --pretty --multipass --config=build.config.yml "{input_}" -o "{output}"')
+
+def svg2png(input_, output):
+    cairosvg.svg2png(
+        url=str(input_),
+        write_to=str(output),
+        output_height=SIZE
+    )
+
+for path in chain(SRC_ICONS.glob('*.svg'), SRC.glob('*.svg')):
+    input_ = DIST / f'{path.stem}.svg'
+    svgo(path, input_)
+    if SIZE is not None:
+        output = DIST / f'{path.stem}.png'
+        svg2png(input_, output)
